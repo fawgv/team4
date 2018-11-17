23448f5a48b16e35cc88cdae2ab2a690bbebf084
fawgv <fawgv@mail.ru>
2018-11-18 00:59:29 +0500

    Add test

--- src/Startup.cs
+++ src/Startup.cs
@@ -24,7 +24,7 @@ namespace thegame
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage()
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvc();
             app.UseSpa(c => { });
