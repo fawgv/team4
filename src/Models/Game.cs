f7c191cc2c0600ebb87257465f1c727ad31688e1
fawgv <fawgv@mail.ru>
2018-11-18 11:33:32 +0500

    Add Content to Game Methods

--- src/Models/Game.cs
+++ src/Models/Game.cs
@@ -7,31 +7,48 @@ namespace thegame.Models
 {
     public class Game
     {
-        private TypeCellGame[,] GameMatrix;
-        
-        public Game(TypeCellGame[,] gameMatrix)
+        private TypeCellGame[,] StaticGameCells;
+        private TypeCellGame[,] DynamicGameCells;
+
+        public Game(TypeCellGame[,] dynamicGameCells)
         {
-            GameMatrix = gameMatrix;
+            DynamicGameCells = dynamicGameCells;
         }
+
+
 
         public TypeCellGame FindCellGameObject(Vec vec)
         {
-            return GameMatrix[vec.X, vec.Y];
+            return DynamicGameCells[vec.X, vec.Y];
         }
 
         public Vec GetPlayerPos()
         {
+            for (int xI = 0; xI < DynamicGameCells.GetLength(0); xI++)
+            {
+                for (int yI = 0; yI < DynamicGameCells.GetLength(1); yI++)
+                {
+                    if (DynamicGameCells[xI, yI] == TypeCellGame.Player)
+                    {
+                        return new Vec(xI, yI);
+                    }
+                }
+            }
             return new Vec(0, 0);
         }
 
         public void MovePlayer(Vec posVec)
         {
-
+            var oldPlayesPos = GetPlayerPos();
+            DynamicGameCells[oldPlayesPos.X, oldPlayesPos.Y] = TypeCellGame.Empty; //Доработать на проверку с StaticGameCells
+            DynamicGameCells[posVec.X, posVec.Y] = TypeCellGame.Player;
         }
+
+
 
         public TypeCellGame[,] GetMap()
         {
-            return new TypeCellGame[0,0];
+            return DynamicGameCells;  //Доработать на проверку с StaticGameCells
         }
 
 
