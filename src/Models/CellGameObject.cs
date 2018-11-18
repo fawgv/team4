13862cb8ff86717ee4b54ac1e2692d4ed14a3a65
fawgv <fawgv@mail.ru>
2018-11-18 10:15:16 +0500

    Add TypeCellGame and Game

---
+++ src/Models/CellGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.Models
+{
+    public enum TypeCellGame
+    {
+        Empty,
+        Box,
+        BoxInTarget,
+        Player,
+        Target,
+        Wall
+    }
+    public class CellGameObject
+    {
+        public TypeCellGame TypeCell { get; set; }
+        public Vec Vec { get; set; }
+    }
+}
---
+++ src/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.Models
+{
+    public class Game
+    {
+        private CellGameObject[,] GameMatrix;
+
+        public Game(CellGameObject[,] gameMatrix)
+        {
+            GameMatrix = gameMatrix;
+        }
+
+        public CellGameObject FindCellGameObject(Vec vec)
+        {
+            return GameMatrix[vec.X, vec.Y];
+        }
+
+
+    }
+}
